e4317126cce8d28ae722a9d50fca706850baee72
Sam Rushing <gitsam@rushing.nightmare.com>
2011-12-26 15:02:02 -0800

    trying a fix for a long-running cython/build_ext issue

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 from setuptools import setup, find_packages, Extension
 from Cython.Distutils import build_ext
 
+# see http://www.velocityreviews.com/forums/t693861-cython-setuptools-not-working-with-pyx-only-with-c-files.html
+try:
+    import Cython
+    # may need to work around setuptools bug by providing a fake Pyrex
+    sys.path.insert(0, os.path.join(os.path.dirname(__file__), "fake_pyrex"))
+except ImportError:
+    pass
+
 setup (
     name             = 'aatree',
     version          = '0.1',
