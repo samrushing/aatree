e541150f539fb373588499712d751126d54d26bd
Sam Rushing <gitsam@rushing.nightmare.com>
2011-12-26 13:35:15 -0800

    github magic for pypi

--- setup.py
+++ setup.py
@@ -4,9 +4,12 @@ from Cython.Distutils import build_ext
 setup (
     name             = 'aatree',
     version          = '0.1',
+    description      = 'simplified variant of the red-black balanced binary search tree',
+    author           = "Sam Rushing",
     packages         = find_packages(),
     ext_modules      = [Extension('aatree.aa', ['aatree/aa.pyx'])],
     cmdclass         = {'build_ext': build_ext},
     install_requires = ['cython>=0.15'],
-    author           = "Sam Rushing",
+    url               = 'http://github.com/samrushing/aatree/',
+    download_url      = "http://github.com/samrushing/aatree/tarball/master#egg=aatree-0.1",
     )
